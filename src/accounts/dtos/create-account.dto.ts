318ba9d7e0c8e25971bce69b59971ad81091ac82
Alireza <alirezahamid20@gmail.com>
2022-01-17 21:52:46 +0000

    remove some comments

--- src/accounts/dtos/create-account.dto.ts
+++ src/accounts/dtos/create-account.dto.ts
@@ -1,12 +1,6 @@
 import { IsNumber, IsString } from 'class-validator';
 
 export class CreateAccountDto {
-  // @IsNumber()
-  // accountNumber: number;
-
-  // @IsNumber()
-  // sortCode: number;
-
   @IsString()
   type: string;
 
